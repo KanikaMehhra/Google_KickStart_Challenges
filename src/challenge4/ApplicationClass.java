60c86b3c78e8a68ccc67ec3d38ebb7df915496e4
Kanika Mehra <s3688154@student.rmit.edu.au>
2020-10-03 21:04:43 +1000

    completed challenge

--- src/challenge4/ApplicationClass.java
+++ src/challenge4/ApplicationClass.java
@@ -3,23 +3,23 @@ package challenge4;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.util.Pair;
+
 public class ApplicationClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("kanika");
 		List<Integer> list1 = Arrays.asList(1, 2);
-		List<Integer> list2 = Arrays.asList(3, 4, 5, 6);
-		List<Integer> list3 = Arrays.asList(7, 8);
+		List<Integer> list2 = Arrays.asList(3, 4, 5, 6, 11, 12);
+		List<Integer> list3 = Arrays.asList(7, 8, 10);
 		List<Integer> list4 = Arrays.asList(9);
 		List<List<Integer>> lists = Arrays.asList(list1, list2, list3, list4);
-		Rectangle rect =new Rectangle(lists);
-//		Matrix matrix = new Matrix(lists);
-		
-		
-
-		// List<List<Integer>> lists=new ArrayList<List<Integer>>([[1, 2], [3, 4, 5, 6],
-		// [7, 8], [9]]);
+		Rectangle rect = new Rectangle(lists);
+		System.out.println("Largest sub of the rectangle is: " + rect.getBiggestRectSum());
+		System.out.println("Array of the largect rectancle indices pair is:");
+		Pair<Integer, Integer>[] pairs = rect.getIndicesPAirArray();
+		for (Pair<Integer, Integer> pair : pairs) {
+			System.out.println("(" + pair.getKey() + ", " + pair.getValue() + ")");
+		}
 	}
 
 }
